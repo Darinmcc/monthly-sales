1ac7ea6649c0298b6e77e3c2d880a35ec03ae737
Darinmcc <50928612+Darinmcc@users.noreply.github.com>
2019-06-19 21:05:08 -0400

    Final

--- reporter.py
+++ reporter.py
@@ -5,6 +5,9 @@ csv_file_path = "data/sales-201710.csv"
 file_date_year = csv_file_path[11:15]
 file_date_month = csv_file_path[15:17]
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
 print(f"GENERATING SALES REPORT FOR MONTH OF OCTOBER {file_date_year}...")
 
 total_sales = 0
@@ -15,11 +18,11 @@ with open(csv_file_path, "r") as csv_file: # "r" means "open the file for readin
     for items in reader:
         
         total_sales += float(items["sales price"] )
+        total_sales_usd = to_usd(total_sales)
     #for row in reader:
         #print(row["date"], row["product"], row["unit price"], row["units sold"], row["sales price"] )
         #date,product,unit price,units sold,sales price
     
-    print(total_sales)
 
 print(f"SALES REPORT (MARCH {file_date_year})")
-print(f"TOTAL SALES: $ {total_sales}")
+print(f"TOTAL SALES: $ {total_sales_usd}")
